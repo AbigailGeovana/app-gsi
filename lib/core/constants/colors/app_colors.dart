4d184afc624817a5ba6550888b9452c23b81d163
abiga <abiga@BIGADESKTOPWIN>
2026-03-13 14:01:45 -0300

    doc: add empty files

---
+++ lib/core/constants/colors/app_colors.dart
@@ -0,0 +1,12 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color blue = Color(0xFF1A237E); // Azul Marinho
+  static const Color orange = Color(0xFFFF9800); // Laranja para destaques
+  static const Color gray = Color(0xFFF5F5F5);   // Fundo levemente cinza
+  static const Color white = Color(0xFFFFFFFF);  // Branco para texto e ícones
+  static const Color black = Color(0xFF000000);  // Preto para texto e ícones
+}
